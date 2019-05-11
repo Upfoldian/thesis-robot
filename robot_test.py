da5a4dd47ffc5e2f388505f5fb8ad1ea3273a072
Thomas Upfold <thomasupfold@gmail.com>
2019-05-11 18:52:35 +1000

    mag test file

--- robot_test.py
+++ robot_test.py
@@ -1,3 +1,17 @@
 import TomLSM303C
 import gpiozero
 import robot
+
+
+robot = robot.Robot()
+
+print("magx, magy, magz")
+for x in range(100000):
+	magx, magy, magz = robot.getMag()
+
+	print("%.2f, %.2f, %.2f" % (magx, magy, magz))
+
+
+
+
+
