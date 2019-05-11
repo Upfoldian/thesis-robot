7765692aa5289f9d2d9a2824e4af985b3746feb6
Thomas Upfold <thomasupfold@gmail.com>
2019-05-11 21:09:42 +1000

    samples had a bug ceebs fixing atm

--- robot.py
+++ robot.py
@@ -123,19 +123,11 @@ class Robot:
 		self.mag = (mag_x, mag_y, mag_z)
 
 	def getHeading(self):
-		avg_x, avg_y, avg_z = 0,0,0
-		samples = 10.0
-		for reading in range(int(samples-1)):
-			accel, mag = self.IMU.read()
-			mag_x, mag_y, mag_z = mag
-			avg_x += mag_x
-			avg_y += mag_y 
-			avg_z += mag_z
-		avg_x /= samples
-		avg_y /= samples
-		avg_z /= samples
+		
+		accel, mag = self.IMU.read()
+		mag_x, mag_y, mag_z = mag
 
-		curHeading = round(degrees(atan2(avg_y, avg_x)), 0) % 360
+		curHeading = round(degrees(atan2(mag_y, mag_x)), 0) % 360
 		return curHeading
 		
 	def getMag(self):
