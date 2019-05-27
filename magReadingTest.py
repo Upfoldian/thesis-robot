301c46b610945d13b09d24a2174bb2d8cd708731
Thomas Upfold <thomasupfold@gmail.com>
2019-05-27 19:16:56 +1000

    mag tests

--- magReadingTest.py
+++ magReadingTest.py
@@ -7,11 +7,9 @@ print("mag_x, mag_y, mag_z")
 prevSample = robot.getMag()
 robot.startLeft(31)
 startTime = time.perf_counter()
-while(time.perf_counter() - startTime > 30):
+while(time.perf_counter() - startTime < 30):
 	curSample = robot.getMag()
 	mag_x, mag_y, mag_z = curSample
-	print("%f, %f, %f" % (mag_x, mag_y, mag_z))
-	print("%f, %f, %f" % (prevSample[0], prevSample[1], prevSample[2]))
 	if ((prevSample[0] != mag_x) or (prevSample[1] != mag_y) or (prevSample[2] != mag_z)):
 		print("%f, %f, %f" % (mag_x, mag_y, mag_z))
 		prevSample = curSample
