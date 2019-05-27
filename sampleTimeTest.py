c7cc3cf2fca8ba253ab19e5fb57252d0a6eb9d10
Thomas Upfold <thomasupfold@gmail.com>
2019-05-27 17:55:32 +1000

    SIGHH

--- sampleTimeTest.py
+++ sampleTimeTest.py
@@ -7,10 +7,13 @@ robot = robot.Robot()
 
 count = 0.0
 pollCount = 0
+print("Starting...")
 while(count < 50):
 	t0 = time.perf_counter()
 
 	x,y,z = robot.getAccel()
+	x2,y2,z2 = robot.getAccel()
+
 	while((x == x2) and (y == y2) and (z == z2)):
 		pollCount += 1
 		x2,y2,z2 = robot.getAccel()
