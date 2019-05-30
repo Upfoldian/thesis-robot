f0acce859e818883db30f479cb8327a17d872ab1
Thomas Upfold <thomasupfold@gmail.com>
2019-05-30 11:54:44 +1000

    ugh

--- colour_mask.py
+++ colour_mask.py
@@ -1,12 +1,14 @@
 import numpy as np
 
 
-teallower	= np.array([90,150,20])
-tealupper	= np.array([110,255,180])
+teallower		= np.array([90,150,20])
+tealupper		= np.array([110,255,180])
 
-bluelower	= np.array([115,220,5])
-blueupper	= np.array([130,255,50])
+bluelower		= np.array([115,220,5])
+blueupper		= np.array([130,255,50])
 
-purplelower	= np.array([215,215,30])
-purpleupper	= np.array([15,255,50])
+purplelower1	= np.array([215,215,30])
+purpleupper1	= np.array([255,255,255])
 
+purplelower2	= np.array([0,0,0])
+purpleupper2	= np.array([15,255,50])
