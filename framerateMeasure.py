43c8ea069b46394d7031122adb1caa857187e7be
Thomas Upfold <thomasupfold@gmail.com>
2019-05-31 21:41:38 +1000

    asdf

--- framerateMeasure.py
+++ framerateMeasure.py
@@ -1,24 +1,29 @@
 import camera
 import time
 
+print("Starting...")
 cam = camera.Camera()
 time.sleep(0.5)
 
 count = 0
 timeSum = 0.0
 
-while (count < 60):
-	tealName = "teal%d" % count
-	yellowName = "yellow%d" % count
-	purpleName = "purple%d" % count
-	combinedName = "all%d" % count
+try:
+	while (count < 60):
+		tealName = "teal%d" % count
+		yellowName = "yellow%d" % count
+		purpleName = "purple%d" % count
+		combinedName = "all%d" % count
 
-	t0 = time.perf_counter()
-	cam.saveImage(tealName, yellowName, purpleName, combinedName)
-	t1 = time.perf_counter()
-	timeSum += t1-t0
+		t0 = time.perf_counter()
+		cam.saveImage(True, tealName, yellowName, purpleName, combinedName)
+		t1 = time.perf_counter()
+		timeSum += t1-t0
 
-timeAvg = (timeSum / 60.0) * 1000.0
+	timeAvg = (timeSum / 60.0) * 1000.0
 
-print("Average FPS: %f" % timeAvg)
-cam.haltThread()
+	print("Average FPS: %f" % timeAvg)
+except:
+	cam.haltThread()
+else:
+	cam.haltThread()
