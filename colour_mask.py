36972910a4b77c917c2f4eddd97977b33c59eb35
Thomas Upfold <thomasupfold@gmail.com>
2019-06-03 21:45:09 +1000

    asdf

--- colour_mask.py
+++ colour_mask.py
@@ -1,14 +1,14 @@
 import numpy as np
 
 
-teallower		= np.array([50,170,20])
+teallower		= np.array([70,170,20])
 tealupper		= np.array([90,255,70])
 
-yellowlower		= np.array([15,135,80])
-yellowupper		= np.array([30,255,125])
+yellowlower		= np.array([90,170,20])
+yellowupper		= np.array([105,255,70])
 
-purplelower1	= np.array([170,230,40])
-purpleupper1	= np.array([200,255,100])
+purplelower1	= np.array([170,205,54])
+purpleupper1	= np.array([255,238,99])
 
 purplelower2	= np.array([0,225,60])
 purpleupper2	= np.array([10,255,117])
