df1d2e4a4941dea7f5dbd84fa9984c4d4d9bb305
Thomas Upfold <thomasupfold@gmail.com>
2019-06-03 19:59:19 +1000

    asf

--- robot.py
+++ robot.py
@@ -47,11 +47,13 @@ class Robot:
 			for target in targets:
 				targetName = target['targetName']
 				x,y,w,h = target['dims']
-				if (abs((x + w/2) - horizontalMidpoint) < 30):
-					#self.motors.stop()
+				if (abs((x + w/2) - horizontalMidpoint) < 20):
+					self.motors.stop()
 					print("Spotted! %s\t x: %d y: %d w: %d h:%d" % (targetName,x,y,w,h))
+					self.motors.stop()
 					self.camera.saveImage(combinedName= ("mask" + targetName), originalName = targetName)
-					time.sleep(2)
+					self.motors.spinRight(0.5)
+					time.sleep(0.2)
 					
 					# lock onto it
 					success = True #self.lockTarget(target)
