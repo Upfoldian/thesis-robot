8b084fbbfdd2d6e65170a23c67d01c58bc3256f5
Thomas Upfold <thomasupfold@gmail.com>
2019-06-03 21:14:59 +1000

    tuxedo mask

--- colour_mask.py
+++ colour_mask.py
@@ -1,14 +1,14 @@
 import numpy as np
 
 
-teallower		= np.array([40,0,10])
-tealupper		= np.array([90,255,60])
+teallower		= np.array([70,80,40])
+tealupper		= np.array([85,255,60])
 
-yellowlower		= np.array([23,215,65])
-yellowupper		= np.array([35,255,150])
+yellowlower		= np.array([17,200,65])
+yellowupper		= np.array([25,255,140])
 
 purplelower1	= np.array([170,0,40])
 purpleupper1	= np.array([200,255,100])
 
 purplelower2	= np.array([0,170,35])
-purpleupper2	= np.array([5,251,83])
+purpleupper2	= np.array([0,251,83])
