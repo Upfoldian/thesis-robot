d2d51ece3c5e1cc62ad7d86451155a6f5d6797af
Thomas Upfold <thomasupfold@gmail.com>
2019-06-03 21:29:33 +1000

    more mask

--- colour_mask.py
+++ colour_mask.py
@@ -7,8 +7,8 @@ tealupper		= np.array([90,255,70])
 yellowlower		= np.array([15,135,80])
 yellowupper		= np.array([30,255,125])
 
-purplelower1	= np.array([170,200,40])
+purplelower1	= np.array([170,230,40])
 purpleupper1	= np.array([200,255,100])
 
-purplelower2	= np.array([0,170,35])
-purpleupper2	= np.array([0,251,83])
+purplelower2	= np.array([0,225,60])
+purpleupper2	= np.array([10,255,117])
