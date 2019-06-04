dbcf398110ac063c3450da9046edcf08bb6f01ff
Upfoldian <thomasupfold@gmail.com>
2019-06-04 09:49:16 +0000

    help

--- udp_send_test.py
+++ udp_send_test.py
@@ -1,12 +1,12 @@
 import socket
-
-
-multicast_group = ("224.1.1.1", 5000)
-MESSAGE = "YO!"
-
-
-print("IP: %s\tPort: %s\tMsg: %s" % (multicast_group[0], multicast_group[1], MESSAGE))
-
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP) # TCP
-sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 32)
-sock.sendto(bytes(MESSAGE, "utf-8"), multicast_group)
+import time
+server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+server.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 5)
+# Set a timeout so the socket does not block
+# indefinitely when trying to receive data.
+server.settimeout(0.2)
+message = b"your very important message"
+while True:
+    server.sendto(message, ('<broadcast>', 5000))
+    print("message sent!")
+    time.sleep(1)
