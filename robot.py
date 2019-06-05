8d9b6fbbfdc3304d44b881f39a2cf158d6480dfc
Thomas Upfold <thomasupfold@gmail.com>
2019-06-06 01:56:38 +1000

    Typo

--- robot.py
+++ robot.py
@@ -88,7 +88,7 @@ class Robot:
 		self.sync()
 		self.comms.send("CLAIM %s %d" % (minTarget, minDist))
 		time.sleep(1)
-		claims = {}
+		claims = []
 		while (self.comms.hasMessage()):
 			msg = self.readMessage()
 			if (msg['opcode'] == "CLAIM"):
